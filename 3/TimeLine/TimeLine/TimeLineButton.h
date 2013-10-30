8e18fb7c34a7b0cfdbe642574932e9792cebe27c
Kolin Weidmann <kolin22@gmail.com>
2013-10-30 22:58:07 +0100

    closest touch

---
+++ 3/TimeLine/TimeLine/TimeLineButton.h
@@ -0,0 +1,13 @@
+//
+//  TimeLineButton.h
+//  TimeLine
+//
+//  Created by Kolin Weidmann on 30/10/13.
+//  Copyright (c) 2013 WEIDMANN Kolin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimeLineButton : UIButton
+
+@end
---
+++ 3/TimeLine/TimeLine/TimeLineButton.m
@@ -0,0 +1,31 @@
+//
+//  TimeLineButton.m
+//  TimeLine
+//
+//  Created by Kolin Weidmann on 30/10/13.
+//  Copyright (c) 2013 WEIDMANN Kolin. All rights reserved.
+//
+
+#import "TimeLineButton.h"
+
+@implementation TimeLineButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
